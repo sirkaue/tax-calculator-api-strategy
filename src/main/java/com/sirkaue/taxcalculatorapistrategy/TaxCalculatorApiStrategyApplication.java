3c6004d9abd6edbc84dfd271ec9587a3ccd22c18
sirkaue <kauekdslopes@gmail.com>
2025-01-31 22:30:46 -0300

    Initial commit

---
+++ src/main/java/com/sirkaue/taxcalculatorapistrategy/TaxCalculatorApiStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.sirkaue.taxcalculatorapistrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalculatorApiStrategyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxCalculatorApiStrategyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirkaue/taxcalculatorapistrategy/TaxCalculatorApiStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirkaue.taxcalculatorapistrategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalculatorApiStrategyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
