dca88f25936aed5d1ed42aa392f57ef1cb5e77dd
sirkaue <kauekdslopes@gmail.com>
2025-06-15 13:47:16 -0300

    refactor: adiciona getType e troca Double por double em ICMSStrategy

--- src/main/java/com/sirkaue/taxcalculatorapistrategy/strategy/tax/ICMSStrategy.java
+++ src/main/java/com/sirkaue/taxcalculatorapistrategy/strategy/tax/ICMSStrategy.java
@@ -1,12 +1,18 @@
 package com.sirkaue.taxcalculatorapistrategy.strategy.tax;
 
+import com.sirkaue.taxcalculatorapistrategy.domain.TaxType;
 import org.springframework.stereotype.Component;
 
-@Component("ICMS")
+@Component
 public class ICMSStrategy implements TaxStrategy {
 
     @Override
-    public Double calculateTax(Double amount) {
+    public TaxType getType() {
+        return TaxType.ICMS;
+    }
+
+    @Override
+    public double calculateTax(double amount) {
         return amount * 0.17; // ICMS de 17%
     }
 }
