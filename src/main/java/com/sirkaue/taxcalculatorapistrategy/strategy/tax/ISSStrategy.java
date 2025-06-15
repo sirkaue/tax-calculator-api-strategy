1bdcd4c65872348dba7bf718ac949905e195d7d9
sirkaue <kauekdslopes@gmail.com>
2025-06-15 13:47:54 -0300

    refactor: adiciona getType e troca Double por double em ISSStrategy

--- src/main/java/com/sirkaue/taxcalculatorapistrategy/strategy/tax/ISSStrategy.java
+++ src/main/java/com/sirkaue/taxcalculatorapistrategy/strategy/tax/ISSStrategy.java
@@ -1,12 +1,18 @@
 package com.sirkaue.taxcalculatorapistrategy.strategy.tax;
 
+import com.sirkaue.taxcalculatorapistrategy.domain.TaxType;
 import org.springframework.stereotype.Component;
 
-@Component("ISS")
+@Component
 public class ISSStrategy implements TaxStrategy {
 
     @Override
-    public Double calculateTax(Double amount) {
+    public TaxType getType() {
+        return TaxType.ISS;
+    }
+
+    @Override
+    public double calculateTax(double amount) {
         return amount * 0.05; // ISS de 5%
     }
 }
