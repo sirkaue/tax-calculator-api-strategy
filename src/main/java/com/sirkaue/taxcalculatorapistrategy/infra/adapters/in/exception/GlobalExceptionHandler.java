24f5bc68d761a699bbb9306606576cdecc83958b
sirkaue <kauekdslopes@gmail.com>
2025-06-22 22:32:22 -0300

    feat: implementar GlobalExceptionHandler para tratar exceções personalizadas e padronizar resposta de erro

---
+++ src/main/java/com/sirkaue/taxcalculatorapistrategy/infra/adapters/in/exception/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.sirkaue.taxcalculatorapistrategy.infra.adapters.in.exception;
+
+import com.sirkaue.taxcalculatorapistrategy.domain.exception.StrategyNotFoundException;
+import com.sirkaue.taxcalculatorapistrategy.domain.exception.TaxTypeNullException;
+import com.sirkaue.taxcalculatorapistrategy.infra.adapters.in.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaxTypeNullException.class)
+    public ResponseEntity<ErrorResponse> handleTaxTypeNull(TaxTypeNullException ex, HttpServletRequest request) {
+        return createErrorResponse(request, HttpStatus.BAD_REQUEST, ex);
+    }
+
+    @ExceptionHandler(StrategyNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleStrategyNotFound(StrategyNotFoundException ex, HttpServletRequest request) {
+        return createErrorResponse(request, HttpStatus.BAD_REQUEST, ex);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(HttpServletRequest request) {
+        Exception formatted = new Exception("Invalid enum value or malformed JSON.");
+        return createErrorResponse(request, HttpStatus.BAD_REQUEST, formatted);
+    }
+
+    private ResponseEntity<ErrorResponse> createErrorResponse(
+            HttpServletRequest request,
+            HttpStatus status,
+            Exception exception) {
+        ErrorResponse error = new ErrorResponse(request, status, exception);
+        return ResponseEntity.status(status).contentType(APPLICATION_JSON).body(error);
+    }
+}
