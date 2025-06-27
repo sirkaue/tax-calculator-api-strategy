ccc264c06c00262da5ea863daae05119cbb9a658
sirkaue <kauekdslopes@gmail.com>
2025-06-27 13:25:46 -0300

    test: garantir definição de estratégia via factory no contexto de imposto

---
+++ src/test/java/com/sirkaue/taxcalculatorapistrategy/unit/application/context/TaxContextImplTest.java
@@ -0,0 +1,40 @@
+package com.sirkaue.taxcalculatorapistrategy.unit.application.context;
+
+import com.sirkaue.taxcalculatorapistrategy.application.context.TaxContextImpl;
+import com.sirkaue.taxcalculatorapistrategy.application.ports.out.factory.TaxStrategyFactory;
+import com.sirkaue.taxcalculatorapistrategy.application.ports.out.strategy.TaxStrategy;
+import com.sirkaue.taxcalculatorapistrategy.domain.enums.TaxType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TaxContextImplTest {
+
+    @Mock
+    private TaxStrategyFactory factory;
+
+    @Mock
+    private TaxStrategy strategy;
+
+    @InjectMocks
+    private TaxContextImpl context;
+
+    @Test
+    void shouldSetStrategyUsingFactory() {
+        // Arrange
+        TaxType taxType = TaxType.ICMS;
+        when(factory.create(taxType)).thenReturn(strategy);
+
+        // Act
+        context.setStrategy(taxType);
+
+        // Assert
+        verify(factory).create(taxType);
+    }
+}
